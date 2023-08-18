af95c75af6f8f8ff800e431049a9b43977a667ff
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2023-08-18 22:11:08 +0100

    winning card

--- index.js
+++ index.js
@@ -21,9 +21,27 @@ function drawCards(){
             child.innerHTML = `<img src='${data.cards[count].image}'/>`
             count++
             }
+            winner(data.cards)
         })
 }
 
+
+function winner(cardsArray){
+    const valuesArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1 = valuesArray.indexOf(cardsArray[0].value)
+    const card2 = valuesArray.indexOf(cardsArray[1].value)
+
+    if(card1 > card2){
+        console.log('card 1 wins!')
+    }else if(card1 < card2){
+        console.log('card 2 wins!')
+    }else{
+        console.log("it's a draw!")
+    }
+
+    
+
+}
 
 
 
