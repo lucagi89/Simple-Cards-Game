acd63ea2f6c79061cfda65c54948e9ad33287585
lucagi89 <106192483+lucagi89@users.noreply.github.com>
2023-08-18 20:13:43 +0100

    develop the game

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 
+const newDeckBtn = document.getElementById('new-deck-btn')
+const drawBtn = document.getElementById('draw-btn')
+let deckId
+
+function getDeck(){
+    fetch('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        .then(res => res.json())
+        .then(data => {
+            deckId = data.deck_id
+        })
+}
+
+function drawCards(){
+    fetch(`https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+}
 
 
-document.getElementById('new-deck-btn').addEventListener('click', () => console.log('clicked'))
+
+
+newDeckBtn.addEventListener('click', getDeck)
+drawBtn.addEventListener('click', drawCards)
